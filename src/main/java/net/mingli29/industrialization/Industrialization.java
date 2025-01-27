ec7a2997ec2b5077312742aaee33a951548a1ae1
William Li <mingli29@outlook.com>
2025-01-27 20:48:47 +0800

    UPDTAE

--- src/main/java/net/mingli29/industrialization/Industrialization.java
+++ src/main/java/net/mingli29/industrialization/Industrialization.java
@@ -2,6 +2,7 @@ package net.mingli29.industrialization;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mingli29.industrialization.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,7 @@ public class Industrialization implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/mingli29/industrialization/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.mingli29.industrialization.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.mingli29.industrialization.Industrialization;
+
+public class ModItems {
+
+    public static final Item STEEL_INGOT = registerItem("steel_ingot",new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(Industrialization.MOD_ID,name),item);
+    }
+    public static void registerModItems(){
+        Industrialization.LOGGER.info("Registering Mod Items For "+Industrialization.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
+            entries.add(STEEL_INGOT);
+        });
+    }
+}
