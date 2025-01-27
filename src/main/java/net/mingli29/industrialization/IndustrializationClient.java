870434df3b2e5f74e82d04542577b91400219489
William Li <mingli29@outlook.com>
2025-01-27 14:01:22 +0800

    BASE

---
+++ src/main/java/net/mingli29/industrialization/IndustrializationClient.java
@@ -0,0 +1,10 @@
+package net.mingli29.industrialization;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IndustrializationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mingli29/industrialization/IndustrializationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mingli29.industrialization;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IndustrializationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
