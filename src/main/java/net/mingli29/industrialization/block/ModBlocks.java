88ed519df9af42eb011291eacb0fc5611638f508
William Li <mingli29@outlook.com>
2025-01-29 16:00:30 +0800

    Language Update 1 Code AL2501295 Block Update 2 Code AB2501291

--- src/main/java/net/mingli29/industrialization/block/ModBlocks.java
+++ src/main/java/net/mingli29/industrialization/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.mingli29.industrialization.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -27,5 +29,9 @@ public class ModBlocks {
 
     public static void registerModBlocks(){
         Industrialization.LOGGER.info("Registering Mod Blocks for "+Industrialization.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
+            entries.add(STEEL_BLOCK);
+        });
     }
 }
